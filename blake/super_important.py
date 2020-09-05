f5a4dba292c212e7208203b1762b8640a2d83f87
MrPowers <matthewkevinpowers@gmail.com>
2020-09-05 06:56:52 -0500

    Add some notebooks

---
+++ blake/super_important.py
@@ -0,0 +1,2 @@
+def some_message():
+    return "I like dancing reggaeton"
---
+++ tests/test_super_important.py
@@ -0,0 +1,6 @@
+import pytest
+
+from blake.super_important import *
+
+def test_some_message():
+    assert some_message() == "I like dancing reggaeton"
